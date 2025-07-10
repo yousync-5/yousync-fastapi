dbb4754f4ffa64e81172f0eef7fd555de1df9192
wonsanha <wonsanha98@gmail.com>
2025-07-10 13:57:19 +0900

    fe_connection

--- back-end/main.py
+++ back-end/main.py
@@ -33,6 +33,7 @@ from router.auth_router import router as auth_router
 from router.actor_router import router as actor_router
 from router.mypage_router import router as mypage_router
 from router.script_audio_router import router as script_audio_router
+from router.url_router import router as url_router
 
 # 데이터베이스 테이블 생성 (앱 시작시 자동으로 테이블이 생성됨)
 Base.metadata.create_all(bind=engine)
@@ -87,6 +88,7 @@ app.include_router(script_router)  # /scripts 경로로 스크립트 관련 API 
 app.include_router(user_audio_router) # /tokens/{token_id}/upload-audio 경로로 유저 음성 데이터 관련 API 등록
 app.include_router(mypage_router) # /mapage 결로로 스크립트 관련 API 등록
 app.include_router(script_audio_router)
+app.include_router(url_router) # url 관련 라우터
 
 # 루트 엔드포인트 - API 서버 상태 확인용
 @app.get("/")
